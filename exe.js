5d9debb269f16f9500d52285cc91d7f851031977
Virgínia Monteiro <virginiadevdesigner@gmail.com>
2025-02-03 15:11:18 -0300

    Add files via upload

---
+++ exe.js
@@ -0,0 +1,3 @@
+console.log(`Meu nome é "Virgínia Monteiro", estou aprendendo JavaScript às '10' da manhã`);
+
+console.log('Meu nome é "Virgínia". Estou aprendendo JS às', 10, 'da manhã.');
---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('Virgínia');//string
+console.log("Monteiro");//string
+console.log('Virginia "Monteiro"');
+console.log(`'Silva' "Nogueira"`)
+
+console.log(35, 15.5, 'Virginia Monteiro');
